0bd4675650a4b0bdb1e7155fabea5b3a604e6fd4
SrinivasMotepalli <148742392+SrinivasMotepalli@users.noreply.github.com>
2023-10-28 22:23:30 +0530

    Create EDA_DPL.py

---
+++ EDA_DPL.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Load data
+df = pd.read_csv("EDA_DPL_DATASET.csv")
+
+# Streamlit App
+st.title('Correlation Analysis of Crimes Against Women')
+
+# Dropdowns for selecting crime types
+crime_type1 = st.selectbox('Select Crime Type 1:', df['Crime_Type'].unique())
+crime_type2 = st.selectbox('Select Crime Type 2:', df['Crime_Type'].unique())
+
+# Scatter plot for correlation analysis
+fig_correlation = px.scatter(df, x=crime_type1, y=crime_type2, title=f'Correlation Analysis: {crime_type1} vs {crime_type2}')
+st.plotly_chart(fig_correlation)
