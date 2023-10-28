a9fe66e5d6e78099cd0c1118a0ba0cd0b94b2fbc
SrinivasMotepalli <148742392+SrinivasMotepalli@users.noreply.github.com>
2023-10-28 23:14:28 +0530

    Update EDA_DPL_CONVICTION_RATE.py

--- EDA_DPL_CONVICTION_RATE.py
+++ EDA_DPL_CONVICTION_RATE.py
@@ -11,11 +11,18 @@ st.title('Conviction Rates for Crimes Against Women')
 crime_type_column = 'Group_Name'
 crime_type = st.selectbox('Select Crime Type:', df[crime_type_column].unique())
 
-# Filter data for the selected crime type
-filtered_df = df[df[crime_type_column] == crime_type]
+# Dropdown for selecting state
+states = ['None'] + sorted(df['Area_Name'].str.strip().unique(), key=str.lower)
+selected_state = st.selectbox('Select State:', states)
+
+# Filter data for state-wise and crime type analysis
+if selected_state == 'None':
+    filtered_df = df[df[crime_type_column] == crime_type]
+else:
+    filtered_df = df[(df['Area_Name'].str.strip().str.lower() == selected_state.lower().strip()) & (df[crime_type_column] == crime_type)]
 
 # Calculate conviction rate
 conviction_rate = max(min((filtered_df['Convicted'].sum() / filtered_df['Arrested'].sum()) * 100, 100), 0)
 
 # Display Conviction Rate as percentage
-st.write(f'Conviction Rate for {crime_type}: {conviction_rate:.2f}%')
+st.write(f'Conviction Rate for {crime_type} in {selected_state}: {conviction_rate:.2f}%')
