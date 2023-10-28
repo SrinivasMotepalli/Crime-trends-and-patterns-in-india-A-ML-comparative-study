52cc8b3036eb413cd66e33bb2192d1c13ba131f7
SrinivasMotepalli <148742392+SrinivasMotepalli@users.noreply.github.com>
2023-10-28 19:48:05 +0530

    Update EDA_DPL_PROJECT.py

--- EDA_DPL_PROJECT.py
+++ EDA_DPL_PROJECT.py
@@ -30,25 +30,27 @@ else:
 
 # Plot the selected graph
 try:
-    if plot_type == 'Line':
-        fig = px.line(filtered_df, x=x_column, y=y_column, title=f'Line Plot: {y_column} over {x_column} - {selected_state}')
-    elif plot_type == 'Bar':
-        fig = px.bar(filtered_df, x=x_column, y=y_column, title=f'Bar Plot: {y_column} vs {x_column} - {selected_state}')
-    elif plot_type == 'Histogram':
-        fig = px.histogram(filtered_df, x=x_column, y=y_column, title=f'Histogram: {y_column} distribution over {x_column} - {selected_state}')
-    elif plot_type == 'Scatter':
-        fig = px.scatter(filtered_df, x=x_column, y=y_column, title=f'Scatter Plot: {y_column} vs {x_column} - {selected_state}')
-    elif plot_type == 'Area':
-        fig = px.area(filtered_df, x=x_column, y=y_column, title=f'Area Plot: {y_column} over {x_column} - {selected_state}')
-    elif plot_type == 'Box':
-        fig = px.box(filtered_df, x=x_column, y=y_column, title=f'Box and Whisker Plot: {y_column} over {x_column} - {selected_state}')
-    elif plot_type == 'Violin':
-        fig = px.violin(filtered_df, x=x_column, y=y_column, title=f'Violin Plot: {y_column} over {x_column} - {selected_state}')
+    if x_column != 'None' and y_column != 'None':
+        if plot_type == 'Line':
+            fig = px.line(filtered_df, x=x_column, y=y_column, title=f'Line Plot: {y_column} over {x_column} - {selected_state}')
+        elif plot_type == 'Bar':
+            fig = px.bar(filtered_df, x=x_column, y=y_column, title=f'Bar Plot: {y_column} vs {x_column} - {selected_state}')
+        elif plot_type == 'Histogram':
+            fig = px.histogram(filtered_df, x=x_column, y=y_column, title=f'Histogram: {y_column} distribution over {x_column} - {selected_state}')
+        elif plot_type == 'Scatter':
+            fig = px.scatter(filtered_df, x=x_column, y=y_column, title=f'Scatter Plot: {y_column} vs {x_column} - {selected_state}')
+        elif plot_type == 'Area':
+            fig = px.area(filtered_df, x=x_column, y=y_column, title=f'Area Plot: {y_column} over {x_column} - {selected_state}')
+        elif plot_type == 'Box':
+            fig = px.box(filtered_df, x=x_column, y=y_column, title=f'Box and Whisker Plot: {y_column} over {x_column} - {selected_state}')
+        elif plot_type == 'Violin':
+            fig = px.violin(filtered_df, x=x_column, y=y_column, title=f'Violin Plot: {y_column} over {x_column} - {selected_state}')
+        else:
+            st.warning("Selected plot type is not supported.")
     else:
-        st.warning("Selected plot type is not supported.")
+        st.warning("Please select valid columns for X-axis and Y-axis.")
 except ValueError as e:
     st.error(f"Error: {e}")
 
 # Show the plot
 st.plotly_chart(fig)
-
