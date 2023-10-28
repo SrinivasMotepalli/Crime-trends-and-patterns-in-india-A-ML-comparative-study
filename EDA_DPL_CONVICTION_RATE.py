880ab9a530d96ea55407e458d950cacc2b022284
SrinivasMotepalli <148742392+SrinivasMotepalli@users.noreply.github.com>
2023-10-28 23:05:32 +0530

    Update EDA_DPL_CONVICTION_RATE.py

--- EDA_DPL_CONVICTION_RATE.py
+++ EDA_DPL_CONVICTION_RATE.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 
 # Load data
 df = pd.read_csv("EDA_DPL_DATASET.csv")
@@ -15,15 +14,8 @@ crime_type = st.selectbox('Select Crime Type:', df[crime_type_column].unique())
 # Filter data for the selected crime type
 filtered_df = df[df[crime_type_column] == crime_type]
 
-# Group by crime type and calculate average conviction rate
-conviction_rates = filtered_df.groupby(crime_type_column)[['Convicted', 'Arrested']].mean().reset_index()
-
 # Calculate conviction rate
-conviction_rates['Conviction_Rate'] = conviction_rates['Convicted'] / conviction_rates['Arrested']
+conviction_rate = (filtered_df['Convicted'].sum() / filtered_df['Arrested'].sum()) * 100
 
-# Bar Plot for Conviction Rates
-fig_conviction_rates = px.bar(conviction_rates, 
-                              x=crime_type_column, 
-                              y='Conviction_Rate', 
-                              title=f'Conviction Rates for {crime_type}')
-st.plotly_chart(fig_conviction_rates)
+# Display Conviction Rate as percentage
+st.write(f'Conviction Rate for {crime_type}: {conviction_rate:.2f}%')
