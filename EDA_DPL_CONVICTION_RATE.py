a5dfa2a4e2bfa55cecee06999570d586a37797ce
SrinivasMotepalli <148742392+SrinivasMotepalli@users.noreply.github.com>
2023-10-28 23:03:04 +0530

    Update EDA_DPL_CONVICTION_RATE.py

--- EDA_DPL_CONVICTION_RATE.py
+++ EDA_DPL_CONVICTION_RATE.py
@@ -9,17 +9,17 @@ df = pd.read_csv("EDA_DPL_DATASET.csv")
 st.title('Conviction Rates for Crimes Against Women')
 
 # Dropdown for selecting crime type
-crime_type = st.selectbox('Select Crime Type:', df.columns.unique())
+crime_type = st.selectbox('Select Crime Type:', df.columns)
 
 # Group by crime type and calculate average conviction rate
-conviction_rates = df.groupby('Crime_Type')[['Convicted', 'Arrested']].mean().reset_index()
+conviction_rates = df.groupby(crime_type)[['Convicted', 'Arrested']].mean().reset_index()
 
 # Calculate conviction rate
 conviction_rates['Conviction_Rate'] = conviction_rates['Convicted'] / conviction_rates['Arrested']
 
 # Bar Plot for Conviction Rates
 fig_conviction_rates = px.bar(conviction_rates, 
-                              x='Crime_Type', 
+                              x=crime_type, 
                               y='Conviction_Rate', 
-                              title='Conviction Rates for Crimes Against Women')
+                              title=f'Conviction Rates for {crime_type}')
 st.plotly_chart(fig_conviction_rates)
