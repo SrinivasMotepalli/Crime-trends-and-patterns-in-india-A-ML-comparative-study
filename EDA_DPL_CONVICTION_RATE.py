4c1ee837c439b91e859c96978b8a25310d3f5069
SrinivasMotepalli <148742392+SrinivasMotepalli@users.noreply.github.com>
2023-11-04 12:28:02 +0530

    Update EDA_DPL_CONVICTION_RATE.py

--- EDA_DPL_CONVICTION_RATE.py
+++ EDA_DPL_CONVICTION_RATE.py
@@ -15,22 +15,14 @@ crime_type = st.selectbox('Select Crime Type:', df[crime_type_column].unique())
 states = ['None'] + sorted(df['Area_Name'].str.strip().unique(), key=str.lower)
 selected_state = st.selectbox('Select State:', states)
 
-# Dropdown for selecting year
-years = ['None'] + sorted(df['Year'].unique())
-selected_year = st.selectbox('Select Year:', years)
-
-# Filter data for state-wise, crime type, and year analysis
+# Filter data for state-wise and crime type analysis
 if selected_state == 'None':
     filtered_df = df[df[crime_type_column] == crime_type]
 else:
     filtered_df = df[(df['Area_Name'].str.strip().str.lower() == selected_state.lower().strip()) & (df[crime_type_column] == crime_type)]
 
-# Filter data for the selected year
-if selected_year != 'None':
-    filtered_df = filtered_df[filtered_df['Year'] == selected_year]
-
 # Calculate conviction rate
 conviction_rate = max(min((filtered_df['Convicted'].sum() / filtered_df['Arrested'].sum()) * 100, 100), 0)
 
 # Display Conviction Rate as percentage
-st.write(f'Conviction Rate for {crime_type} in {selected_state} in {selected_year if selected_year != "None" else "all years"}: {conviction_rate:.2f}%')
+st.write(f'Conviction Rate for {crime_type} in {selected_state}: {conviction_rate:.2f}%')
