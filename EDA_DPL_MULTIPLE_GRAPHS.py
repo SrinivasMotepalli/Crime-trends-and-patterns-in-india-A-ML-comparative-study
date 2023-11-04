64738f517cb4aa4ba0a7416a5a3e00e40d76dd4b
SrinivasMotepalli <148742392+SrinivasMotepalli@users.noreply.github.com>
2023-11-04 12:08:55 +0530

    Update EDA_DPL_MULTIPLE_GRAPHS.py

--- EDA_DPL_MULTIPLE_GRAPHS.py
+++ EDA_DPL_MULTIPLE_GRAPHS.py
@@ -14,28 +14,36 @@ x_column = st.selectbox('Select X-axis Column:', df.columns)
 y_column = st.selectbox('Select Y-axis Column:', df.columns)
 
 # Dropdown for selecting plot type
-plot_type = st.selectbox('Select Plot Type:', [ 'Bar', 'Histogram', 'Scatter', 'Area'])
+plot_type = st.selectbox('Select Plot Type:', ['Bar', 'Histogram', 'Scatter', 'Area'])
 
 # Dropdown for selecting state
 states = ['None'] + sorted(df['Area_Name'].str.strip().unique(), key=str.lower)
 selected_state = st.selectbox('Select State:', states)
 
-# Filter data for state-wise analysis
+# Dropdown for selecting year
+years = ['None'] + sorted(df['Year'].unique())
+selected_year = st.selectbox('Select Year:', years)
+
+# Filter data for state-wise and year-wise analysis
 if selected_state == 'None':
     filtered_df = df
 else:
     filtered_df = df[df['Area_Name'].str.strip().str.lower() == selected_state.lower().strip()]
 
+# Filter data for the selected year
+if selected_year != 'None':
+    filtered_df = filtered_df[filtered_df['Year'] == selected_year]
+
 # Plot the selected graph
 try:
     if plot_type == 'Bar':
-        fig = px.bar(filtered_df, x=x_column, y=y_column, title=f'Bar Plot: {y_column} vs {x_column} - {selected_state}')
+        fig = px.bar(filtered_df, x=x_column, y=y_column, title=f'Bar Plot: {y_column} vs {x_column} - {selected_state} - {selected_year}')
     elif plot_type == 'Histogram':
-        fig = px.histogram(filtered_df, x=x_column, y=y_column, title=f'Histogram: {y_column} distribution over {x_column} - {selected_state}')
+        fig = px.histogram(filtered_df, x=x_column, y=y_column, title=f'Histogram: {y_column} distribution over {x_column} - {selected_state} - {selected_year}')
     elif plot_type == 'Scatter':
-        fig = px.scatter(filtered_df, x=x_column, y=y_column, title=f'Scatter Plot: {y_column} vs {x_column} - {selected_state}')
+        fig = px.scatter(filtered_df, x=x_column, y=y_column, title=f'Scatter Plot: {y_column} vs {x_column} - {selected_state} - {selected_year}')
     elif plot_type == 'Area':
-        fig = px.area(filtered_df, x=x_column, y=y_column, title=f'Area Plot: {y_column} over {x_column} - {selected_state}')
+        fig = px.area(filtered_df, x=x_column, y=y_column, title=f'Area Plot: {y_column} over {x_column} - {selected_state} - {selected_year}')
     else:
         st.warning("Selected plot type is not supported.")
 except ValueError as e:
@@ -43,3 +51,4 @@ except ValueError as e:
 
 # Show the plot
 st.plotly_chart(fig)
+
